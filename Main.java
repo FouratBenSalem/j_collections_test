a68593e21b577a38979151b680d6926b402c400b
fouratbs <fourat.bensalem8b3@gmail.com>
2020-11-05 03:45:45 +0100

    Added Stack

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
-import java.util.LinkedList;
+import java.util.Stack;
 import java.util.ListIterator;
 
 
 /*
-    ArrayList : mono-thread, accès direct, optimisée pour la lecture
-    LinkedList : insertions rapides
-    Vector : multi-thread, accès direct, optimisée pour la lecture
-    Stack : LIFO
+    1)ArrayList : mono-thread, accès direct, optimisée pour la lecture
+    2)LinkedList : insertions rapides
+    3)Vector : multi-thread, accès direct, optimisée pour la lecture
+      |-> 4)Stack : LIFO
 
     Informations Ordonnées, acceptent les valeurs Nulles et les doublons
     
-    à prendre en consideration : 
-        Capacité != Taile
-        Taille <= Capacité
+    [!] à prendre en consideration : 
+            Capacité != Taile
+            Taille <= Capacité
 
-    add(), addAll(), get(),remove(), clear(), size(), //Vectors : capacity()
+    * Collections Methods : 
+        add(), addAll(), get(),remove(), clear(), size(), isEmpty(), empty(), //Vectors : capacity()
 
-    LinkedList methods : 
+    * LinkedList Methods : 
         addFirst(), addLast, removeFirst(), removeLast(), getFirst(), getLast()
 */
 
@@ -24,20 +25,23 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        LinkedList<Cat> group = new LinkedList<Cat>();
+        Stack<Cat> group = new Stack<Cat>();
 
         Cat c1 = new Cat("Toudoux");
         Cat c2 = new Cat("Popy");
         Cat c3 = new Cat("Maki");
 
-        group.addFirst(c1);
-        group.addFirst(c2);
-        group.addFirst(c3);
+        group.push(c1);
+        group.push(c2);
+        group.push(c3);
 
         ListIterator<Cat> lit = group.listIterator();
 
         while(lit.hasNext())
             lit.next().meow();
+
+        System.out.println("\nSommet de la liste : "+group.peek());
+        group.peek().meow();
 
 
         
