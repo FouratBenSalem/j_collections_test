be950a5ea4c688d83f1c1b9095a329dd2c128fa6
fouratbs <fourat.bensalem8b3@gmail.com>
2020-11-05 01:40:44 +0100

    created Cat class & deleted Cats& added 3 instances

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args) 
     {
-    
+        Cat c1 = new Cat("Toudoux");
+        Cat c2 = new Cat("Popy");
+        Cat c3 = new Cat("Maki");
+
     }
 }
